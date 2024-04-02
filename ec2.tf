8e5ef134d1d4f5f8c2eaaae0420a184cbc20d5d4
gabousmen <gabousmen@hotmail.com>
2024-04-02 17:56:42 -0400

    ec2 name change

--- ec2.tf
+++ ec2.tf
@@ -19,22 +19,22 @@
 #   tags = merge({ Name = local.ec2-test.name, DeployedBy = local.ec2-test.DeployedBy }, local.tags)
 # }
 
-resource "aws_instance" "ec2-app" {
-  ami           = local.ec2-app.ami
-  instance_type = local.ec2-app.inst_type
+resource "aws_instance" "ec2-appa" {
+  ami           = local.ec2-appa.ami
+  instance_type = local.ec2-appa.inst_type
   subnet_id     = module.vpc.private_subnets[0]
   vpc_security_group_ids = [
     aws_security_group.tdts.id, aws_security_group.onprem.id, aws_security_group.swinds.id, aws_security_group.virtusa.id, aws_security_group.zscalar.id
   ]
   root_block_device {
-    delete_on_termination = local.ec2-app.root_device.delete_on_termination
-    encrypted             = local.ec2-app.root_device.encrypted
-    iops                  = local.ec2-app.root_device.iops
-    throughput            = local.ec2-app.root_device.throughput
-    volume_size           = local.ec2-app.root_device.volume_size
-    volume_type           = local.ec2-app.root_device.volume_type
-    tags                  = merge({ Name = local.ec2-app.name, DeployedBy = local.ec2-app.DeployedBy }, local.tags)
+    delete_on_termination = local.ec2-appa.root_device.delete_on_termination
+    encrypted             = local.ec2-appa.root_device.encrypted
+    iops                  = local.ec2-appa.root_device.iops
+    throughput            = local.ec2-appa.root_device.throughput
+    volume_size           = local.ec2-appa.root_device.volume_size
+    volume_type           = local.ec2-appa.root_device.volume_type
+    tags                  = merge({ Name = local.ec2-appa.name, DeployedBy = local.ec2-appa.DeployedBy }, local.tags)
   }
-  tags = merge({ Name = local.ec2-app.name, DeployedBy = local.ec2-app.DeployedBy }, local.tags)
+  tags = merge({ Name = local.ec2-appa.name, DeployedBy = local.ec2-appa.DeployedBy }, local.tags)
 }
 
